90daba0722c002600786e412e2d41bdb075d2f1a
SkakunOstap <skakunostap@gmail.com>
2025-08-26 21:59:04 +0300

    Add help command

--- handlers/start.py
+++ handlers/start.py
@@ -11,6 +11,12 @@ async def start(message: types.Message):
     await message.answer('<b>Hello, world!</b>\nTo get yourself a schedule, use one of the given commands\n/week_schedule\n/interval_schedule', parse_mode=ParseMode.HTML)
 
 
+@router.message(Command("help"))
+async def cancel_state(message: types.Message, state: FSMContext):
+    await message.answer('Available Commands:\n/help – Show this help message\n/week_schedule – Get a schedule for a specific week number\n/interval_schedule – Get a schedule for a custom date range\n/cancel – Cancel the current action and start over')
+    await state.clear()
+
+
 @router.message(Command("cancel"))
 async def cancel_state(message: types.Message, state: FSMContext):
     await message.answer('Canceled current action')
